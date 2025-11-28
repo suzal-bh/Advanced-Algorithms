5d2a7087b937d0ce948a0473412af565eea502d2
suzal-bh <suzal@bhattarai.co.uk>
2025-11-28 18:57:20 +0000

    final changes to Q_1.py

--- q_1.py
+++ q_1.py
@@ -1,3 +1,4 @@
+#Q_1: with 2 recursive calls
 from random import randint
 
 def fun(n):
@@ -31,5 +32,32 @@ def fun(n):
 print(fun(4))
 
 
+# Q_1: with only one recursive call
+def fun(n):
+    luck = 0
+    #roll a dice randomly 
+    dice = randint(1, 6)
+    
+    if dice == 6:
+        luck = 1
+    
+    if n == 1:
+        return 1, 0, luck
+    if n == 2:
+        return 2, 1, luck
+    
+    #only one recursive call
+    first_result = fun(n - 1)
+    
+    first_entry, second_entry, first_luck = first_result
 
+    y = first_entry + 2 * second_entry
+
+    luck += first_luck 
+
+    #return the result as a tuples
+    return y, first_entry, luck
+
+result = fun(4)
+print(result[0], result[2])
 
